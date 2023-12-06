026984ee0fad05ca4e4419a796e2641e7ebf0d1a
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2023-12-06 18:04:42 +0000

    Refactor scanner service and add logger

--- core/src/bounded-contexts/scanner/scanner.service.ts
+++ core/src/bounded-contexts/scanner/scanner.service.ts
@@ -1,9 +1,31 @@
-import { multiInject } from "inversify";
+import { inject, injectable, multiInject } from "inversify";
 import type { ScannerPort } from "../../libs/ports/scanner.port";
+import * as _ from "../..";
 
+@injectable()
 export class ScannerService {
+    protected readonly logger;
+
     constructor(
         @multiInject("ScannerPort")
         protected readonly scanners: ScannerPort[],
-    ) {}
+
+        @inject(_.DI_LOGGER_FACTORY)
+        loggerFactory: _.LoggerFactory,
+    ) {
+        this.logger = loggerFactory("service.scanner");
+    }
+
+    async scanIP(ip: string): Promise<void> {
+        this.logger.info("Scanning...");
+
+        const scanPromises = this.scanners.map((scanner) => {
+            this.logger.debug(`Scanning with ${scanner.constructor.name}`);
+            return scanner.scanIP(ip);
+        });
+
+        await Promise.all(scanPromises);
+
+        this.logger.info("Scanning done.");
+    }
 }
--- core/src/libs/ports/index.ts
+++ core/src/libs/ports/index.ts
@@ -1,1 +1,2 @@
+export * from "./scanner.port";
 export * from "./logger.port";
