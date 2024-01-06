0923fe9db5a7d4caaccbb4e81df799a5a8c3b059
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-01-06 21:02:49 +0000

    Enhance development script and add new dependencies
    Updated 'dev' script in package.json to include a '--turbo' option for faster development. Added '@radix-ui/react-slot' to dependencies, updated next.config.mjs for conditionally implementing 'swcMinify' in production mode, and made significant additions to the Home function in src/app/page.tsx to enrich the webpage content.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@
  */
 await import("./src/env.mjs");
 
+const isProduction = process.env.NODE_ENV === "production"
+
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    swcMinify: isProduction
+};
 
 export default config;
