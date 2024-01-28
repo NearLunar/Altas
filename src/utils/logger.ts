87fba14e684b001ee39697953c858844af132af5
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-01-28 13:01:14 +0000

    Improve Winston logger setup for environment-specific behavior
    This commit adds environment-specific adjustments to Winston logger's settings. It segregates the development and production environment log level, where the debug level is set for the development phase while the info level is chosen for production. Furthermore, this also provides a formatted colorized output specifically for the development phase.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,30 @@
 // Setup Winston logger
 import { createLogger, format, transports } from "winston";
 
+import { env } from "@/env";
+
+const isProduction = env.NODE_ENV === "production";
+
+const devFormat = format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.simple(),
+    format.printf((info) => {
+        const { timestamp, level, message, ...args } = info;
+
+        const ts = timestamp.slice(0, 19).replace("T", " ");
+        return `${ts} [${level}]: ${message.trim()} ${
+            Object.keys(args).length ? JSON.stringify(args, null, 2) : ""
+        }`;
+    }),
+);
+
 export const logger = createLogger({
-    level: "info",
+    level: isProduction ? "info" : "debug",
     format: format.combine(format.timestamp(), format.json()),
     transports: [
         new transports.Console({
-            format: format.combine(format.colorize(), format.simple()),
+            format: devFormat,
         }),
     ],
 });
