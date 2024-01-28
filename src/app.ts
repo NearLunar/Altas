709039157c9052a4d8d54fbbada889e274a072de
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-01-28 12:59:05 +0000

    Update Express app to use additional middleware
    The commit adds new middleware to the Express application for better data parsing and security. It utilizes 'cookie-parser' for parsing cookie bodies, 'urlencoded' and 'json' for parsing data bodies, and 'helmet' to set various security headers. The default route is also updated to use the 'MainRouter'.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
+import cookieParser from "cookie-parser";
 import type { Express } from "express";
-import express from "express";
+import express, {
+    json as jsonParser,
+    urlencoded as urlEncodedParser,
+} from "express";
+import helmet from "helmet";
+
+import { MainRouter } from "@/routes";
 
 const app: Express = express();
 
-app.use(express.json());
+app.use(jsonParser()); // Parse JSON bodies
+app.use(urlEncodedParser({ extended: true })); // Parse URL-encoded bodies
+app.use(cookieParser()); // Parse cookie bodies
+app.use(helmet()); // Add security headers
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// Use Main Router
+app.use("/", MainRouter);
 
 export { app };
