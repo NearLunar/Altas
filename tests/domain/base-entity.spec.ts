c83523d7fcfaa95be1f8c43307051c6d3b5f3a89
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2024-01-28 22:37:31 +0000

    Add BaseEntity class and corresponding tests
    This commit introduces a new 'BaseEntity' class within the abstracts folder which provides a generic structure for entity classes. It also implements validation checks for base properties. Corresponding tests have been added to validate the functionality of this new base entity class.

---
+++ tests/domain/base-entity.spec.ts
@@ -0,0 +1,98 @@
+import { BaseEntity } from "@/abstracts/base-entity";
+
+class TestEntity extends BaseEntity<unknown> {
+    validate() {
+        // Do nothing
+    }
+}
+
+const MaxDate = new Date(8640000000000000);
+const MinDate = new Date(-8640000000000000);
+
+describe("base entity", () => {
+    it("should fail validation for id on initialisation", () => {
+        expect(
+            () =>
+                new TestEntity({
+                    id: "invalid-id",
+                    props: {},
+                }),
+        ).toThrow("Invalid ID");
+    });
+
+    it("should fail validation for createdAt on initialisation", () => {
+        expect(
+            () =>
+                new TestEntity({
+                    createdAt: MaxDate,
+                    props: {},
+                }),
+        ).toThrow("Invalid Date");
+
+        expect(
+            () =>
+                new TestEntity({
+                    createdAt: MinDate,
+                    props: {},
+                }),
+        ).not.toThrow();
+    });
+
+    it("should fail validation for updatedAt on initialisation", () => {
+        expect(
+            () =>
+                new TestEntity({
+                    updatedAt: MaxDate,
+                    props: {},
+                }),
+        ).toThrow("Invalid Date");
+
+        expect(
+            () =>
+                new TestEntity({
+                    updatedAt: MinDate,
+                    props: {},
+                }),
+        ).not.toThrow();
+    });
+
+    it("should fail validation for props on initialisation", () => {
+        expect(
+            () =>
+                new TestEntity({
+                    props: {
+                        invalid1: "props",
+                        invalid2: "props",
+                        invalid3: "props",
+                        invalid4: "props",
+                        invalid5: "props",
+                        invalid6: "props",
+                        invalid7: "props",
+                        invalid8: "props",
+                        invalid9: "props",
+                        invalid10: "props",
+                        invalid11: "props",
+                        invalid12: "props",
+                        invalid13: "props",
+                        invalid14: "props",
+                    },
+                }),
+        ).toThrow("Invalid Props");
+
+        expect(
+            () =>
+                new TestEntity({
+                    props: {},
+                }),
+        ).not.toThrow();
+    });
+
+    it("shouldn't fail on default initialization", () => {
+        expect(
+            () =>
+                new TestEntity({
+                    props: {},
+                }),
+        ).not.toThrow();
+    });
+});
